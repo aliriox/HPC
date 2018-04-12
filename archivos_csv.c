3f23c6b773e785084ee196e14eeba76297c1772b
aliriox <aliriox@utp.edu.co>
2018-04-12 17:45:48 -0500

    arreglado la violacion del segmento

--- archivos_csv.c
+++ archivos_csv.c
@@ -1,39 +1,56 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define N 10000
+
+int Detected_rows(FILE *fp, char nombre[]){
+
+	fp = fopen(nombre,"r");
+
+	int i = 0;
+	char c[N];
+	while(fscanf(fp,"%s",c) != EOF){
+		i++;
+	}
+	fclose(fp);
+	return i;
+}
+
+int Detected_columns(FILE *fp, char nombre[100]){
+
+	fp = fopen(nombre, "r");
+	char cadena[N];
+	fscanf(fp,"%s",cadena);
+
+	char *tok = strtok(cadena, ";");
+	int i = 0;
+	do{
+		i++;
+		tok = strtok(NULL, ";");
+	}while(tok != NULL);
+	fclose(fp);
+
+	return i;
+}
 
 int main(int argc, char *argv[]){
+
+	char nombre[N] = "entrada1omp.csv";
 	
 	FILE *fp;
-	fp = fopen("entradaM1.csv", "r");
+	fp = fopen(nombre, "r");
 	
 	if(fp == NULL){
 		printf("archivo no encontrado");
 		return 1;
 	}
-	
-	char cadena[100];
-	fscanf(fp, "%s", cadena);
-	printf("%s", cadena);
 	fclose(fp);
-	
-	printf("\n");
-	
-	// metodo para separar los datos con ;
-	
-	char *tok = strtok(cadena, ";");
 
-	int i = 1;
-	
-	do{
-		printf("%s\n",tok);
-		i++;
-		tok = strtok(NULL, ";");
-	}while(tok != NULL);
-	
-	sprintf("El numero de columnas es: %d\n", i);
-	
-	printf("\n");
+	int rows_1 = Detected_rows(fp,nombre);
+	printf("El numero de filas es: %d\n", rows_1);
+
+	int columns_1 = Detected_columns(fp,nombre);
+	printf("El numero de columnas es: %d\n", columns_1);
 	
 	/* metodo de transformacion string a float
 		
